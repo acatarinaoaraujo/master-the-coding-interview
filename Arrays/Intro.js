9ae13267f6d5fb6702116f3248ab7966fe1adaf1
acatarinaoaraujo <acoa_@hotmail.com>
2022-06-09 19:49:07 -1000

    Intro to Arrays

---
+++ Arrays/Intro.js
@@ -0,0 +1,17 @@
+/** Variable array is somewhere in memory and the computer knows it.
+ When I do array[2], i'm telling the computer, hey go to the array and
+ grab the 3rd item from where the array is stored. **/
+
+const strings= ['a', 'b', 'c', 'd'];
+const numbers = [1,2,3,4,5];
+
+strings.push('e');
+
+strings.pop();
+strings.pop();
+
+strings.unshift('x')
+
+strings.splice(2, 0, 'alien');
+
+console.log(strings)
