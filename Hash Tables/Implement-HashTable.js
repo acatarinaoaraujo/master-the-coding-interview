801cd49fd10e8e3331d0633e288315cb1c986ecf
acatarinaoaraujo <acoa_@hotmail.com>
2022-06-10 15:03:05 -1000

    Implement Hash Table

--- Hash Tables/Implement-HashTable.js
+++ Hash Tables/Implement-HashTable.js
@@ -36,10 +36,38 @@ class HashTable {
     }
     return undefined;
   }
+
+  /* If we do this.data[i], we get arrays within arrays because of the set method where we do
+  this.data[address] = 0, ex: [['apples, 54]]. Thus, we use this.data[i][0][0] instead, the first
+   [0] to remove the outter array, ex:['apples, 54], and the second [0] for the first index.  */
+
+  keys() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+      // if it's not an empty memory cell
+      if (this.data[i] && this.data[i].length) {
+        // but also loop through all the potential collisions
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0])
+          }
+        } else {
+          result.push(this.data[i][0])
+        }
+      }
+    }
+    return result;
+  }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000)
+myHashTable.set('grapes', 10000)
 myHashTable.get('grapes')
 myHashTable.set('apples', 9)
 myHashTable.get('apples')
+console.log(myHashTable.keys())
